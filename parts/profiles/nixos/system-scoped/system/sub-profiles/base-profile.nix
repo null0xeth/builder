a5efcc5bd1720e2f26cc9256b6801f9789e5a2e2
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-01-07 14:15:08 +0100

    .

--- parts/profiles/nixos/system-scoped/system/sub-profiles/base-profile.nix
+++ parts/profiles/nixos/system-scoped/system/sub-profiles/base-profile.nix
@@ -159,7 +159,7 @@ in {
           fonts = {
             enableDefaultPackages = true;
             inherit (cfg.fonts) packages;
-            fontconfig.defaultFonts = cfg.fonts.defaults;
+            fontconfig.defaultFonts = optionalAttrs (cfg.fonts.defaults != {}) cfg.fonts.defaults;
           };
         })
 
