d050a2eb13fa53b5dd599b33696e0de6e1cb13de
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-01-07 07:15:03 +0100

    .

--- parts/profiles/nixos/system-scoped/hardware/sub-profiles/cpu/default.nix
+++ parts/profiles/nixos/system-scoped/hardware/sub-profiles/cpu/default.nix
@@ -66,11 +66,11 @@ in {
       ];
     }
     (mkIf (cfg.profile.enable && (cfg.profile.cpu != null)) {
-      hardware-cpu-presets = {
-        "${cfg.profile.cpu.brand}-${cfg.profile.cpu.sub-type}-${builtins.toString cfg.profile.cpu.generation}th" = {
-          enable = true;
-        };
-      };
+      # hardware-cpu-presets = {
+      #   "${cfg.profile.cpu.brand}-${cfg.profile.cpu.sub-type}-${builtins.toString cfg.profile.cpu.generation}th" = {
+      #     enable = true;
+      #   };
+      # };
     })
 
     # hardware-cpu-presets = let
