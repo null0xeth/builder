2526f03f819bb6e2d3a2bae227763561345d63ee
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-01-07 13:22:41 +0100

    .

--- parts/profiles/nixos/system-scoped/kernel/sub-profiles/base-profile.nix
+++ parts/profiles/nixos/system-scoped/kernel/sub-profiles/base-profile.nix
@@ -13,192 +13,201 @@ with lib; let
       enable = mkEnableOption "";
     };
   };
-
-  profileTemplate = {name, ...}: {
-    options = {
-      name = mkOption {
-        type = types.str;
-        default = "nakedTemplate";
-        description = mdDoc "Capybara";
-      };
-
-      enable = mkEnableOption "the default kernel profile template";
-      general = mkOption {
-        default = {};
-        type = types.submodule {
-          options = {
-            enable = mkEnableOption (mdDoc "the general kernel configuration module");
-            useLatest = mkEnableOption "the latest kernel packages";
-            kernelPackages = mkOption {
-              type = types.raw;
-              description = "If `useLatest` is disabled, specify the packages here";
-              default = pkgs.linuxPackages_latest;
-            };
-            kernelModules = mkOption {
-              type = types.nullOr (types.listOf types.str);
-              description = "Kernel modules to be installed";
-              default = [];
-            };
-            kernelParams = mkOption {
-              default = {};
-              type = types.submodule {
-                options = {
-                  useDefaults = mkEnableOption "the default kernel parameters";
-                  customParams = mkOption {
-                    type = types.nullOr (types.listOf types.str);
-                    description = "Kernel parameters";
-                    default = [];
-                  };
-                };
-              };
-            };
-            initrd = mkOption {
-              default = {};
-              type = types.submodule {
-                options = {
-                  systemd = mkOption {
-                    default = {};
-                    type = enableModule;
-                  };
-                  kernelModules = mkOption {
-                    type = types.nullOr (types.listOf types.str);
-                    description = "Kernel modules to always be installed";
-                    default = [];
-                  };
-                  availableKernelModules = mkOption {
-                    type = types.nullOr (types.listOf types.str);
-                    description = "Kernel modules to be installed";
-                    default = [];
-                  };
-                };
-              };
-            };
-          };
-        };
-      };
-
-      tweaks = mkOption {
-        default = {};
-        type = types.submodule {
-          options = {
-            networking = mkOption {
-              default = {};
-              type = enableModule;
-            };
-            hardening = mkOption {
-              default = {};
-              type = enableModule;
-            };
-            failsaves = mkOption {
-              default = {};
-              type = enableModule;
-            };
-            clean = mkOption {
-              default = {};
-              type = enableModule;
-            };
-          };
-        };
-      };
-
-      boot = mkOption {
-        default = {};
-        type = types.submodule {
-          options = {
-            enable = mkEnableOption "blabla";
-            general = mkOption {
-              default = {};
-              type = types.submodule {
-                options = {
-                  silent = mkOption {
-                    default = {};
-                    type = types.submodule {
-                      options = {
-                        enable = mkEnableOption "tbalol";
-                      };
-                    };
-                  };
-                };
-              };
-            };
-            tmpfs = mkOption {
-              default = {};
-              type = types.submodule {
-                options = {
-                  enable = mkEnableOption "the temporary filesystem";
-                  size = mkOption {
-                    type = types.nullOr types.str;
-                    default = null;
-                  };
-                };
-              };
-            };
-            loader = mkOption {
-              default = {};
-              type = types.submodule {
-                options = {
-                  systemd = mkOption {
-                    default = {};
-                    type = types.submodule {
-                      options = {
-                        enable = mkEnableOption "systemd boot for this system";
-                        configurationLimit = mkOption {
-                          type = types.nullOr types.int;
-                          default = 5;
-                          description = mdDoc "the maximum number of nixos generations kept on this system";
-                        };
-                      };
-                    };
-                  };
-                  settings = mkOption {
-                    default = {};
-                    type = types.submodule {
-                      options = {
-                        efiSupport = mkOption {
-                          default = {};
-                          type = enableModule;
-                        };
-                        timeout = mkOption {
-                          type = types.int;
-                          default = 3;
-                          description = mdDoc "the maximum allowed time in seconds to time out kernel operations";
-                        };
-                        copyToTmpfs = mkOption {
-                          default = {};
-                          type = enableModule;
-                        };
-                      };
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
-      };
-
-      optionals = mkOption {
-        default = {};
-        type = types.submodule {
-          options = {
-            ricemyttydotcom = mkOption {
-              default = {};
-              type = enableModule;
-            };
-          };
-        };
-      };
-    };
-  };
 in {
   imports = [./submodules];
 
   options.profiles.kernel = {
     preset = mkOption {
       default = {};
-      type = types.attrsOf (types.submodule profileTemplate);
+      type = types.attrsOf (types.submodule ({name, ...}: {
+        options = {
+          name = mkOption {
+            type = types.str;
+            default = "nakedTemplate";
+            description = mdDoc "Capybara";
+          };
+
+          enable = mkEnableOption "the default kernel profile template";
+
+          general = mkOption {
+            default = {};
+            type = types.submodule {
+              options = {
+                enable = mkEnableOption (mdDoc "the general kernel configuration module");
+                useLatest = mkEnableOption "the latest kernel packages";
+                kernelPackages = mkOption {
+                  type = types.raw;
+                  description = "If `useLatest` is disabled, specify the packages here";
+                  default = pkgs.linuxPackages_latest;
+                };
+                kernelModules = mkOption {
+                  type = types.nullOr (types.listOf types.str);
+                  description = "Kernel modules to be installed";
+                  default = [];
+                };
+                kernelParams = mkOption {
+                  default = {};
+                  type = types.submodule {
+                    options = {
+                      useDefaults = mkEnableOption "the default kernel parameters";
+                      customParams = mkOption {
+                        type = types.nullOr (types.listOf types.str);
+                        description = "Kernel parameters";
+                        default = [];
+                      };
+                    };
+                  };
+                };
+                initrd = mkOption {
+                  default = {};
+                  type = types.submodule {
+                    options = {
+                      systemd = mkOption {
+                        default = {};
+                        type = enableModule;
+                      };
+                      kernelModules = mkOption {
+                        type = types.nullOr (types.listOf types.str);
+                        description = "Kernel modules to always be installed";
+                        default = [];
+                      };
+                      availableKernelModules = mkOption {
+                        type = types.nullOr (types.listOf types.str);
+                        description = "Kernel modules to be installed";
+                        default = [];
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+
+          tweaks = mkOption {
+            default = {};
+            type = types.submodule {
+              options = {
+                networking = mkOption {
+                  default = {};
+                  type = enableModule;
+                };
+                hardening = mkOption {
+                  default = {};
+                  type = enableModule;
+                };
+                failsaves = mkOption {
+                  default = {};
+                  type = enableModule;
+                };
+                clean = mkOption {
+                  default = {};
+                  type = enableModule;
+                };
+              };
+            };
+          };
+
+          boot = mkOption {
+            default = {};
+            type = types.submodule {
+              options = {
+                enable = mkEnableOption "blabla";
+                general = mkOption {
+                  default = {};
+                  type = types.submodule {
+                    options = {
+                      silent = mkOption {
+                        default = {};
+                        type = types.submodule {
+                          options = {
+                            enable = mkEnableOption "tbalol";
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+                tmpfs = mkOption {
+                  default = {};
+                  type = types.submodule {
+                    options = {
+                      enable = mkEnableOption "the temporary filesystem";
+                      size = mkOption {
+                        type = types.nullOr types.str;
+                        default = null;
+                      };
+                    };
+                  };
+                };
+                loader = mkOption {
+                  default = {};
+                  type = types.submodule {
+                    options = {
+                      systemd = mkOption {
+                        default = {};
+                        type = types.submodule {
+                          options = {
+                            enable = mkEnableOption "systemd boot for this system";
+                            configurationLimit = mkOption {
+                              type = types.nullOr types.int;
+                              default = 5;
+                              description = mdDoc "the maximum number of nixos generations kept on this system";
+                            };
+                          };
+                        };
+                      };
+                      settings = mkOption {
+                        default = {};
+                        type = types.submodule {
+                          options = {
+                            efiSupport = mkOption {
+                              default = {};
+                              type = enableModule;
+                            };
+                            timeout = mkOption {
+                              type = types.int;
+                              default = 3;
+                              description = mdDoc "the maximum allowed time in seconds to time out kernel operations";
+                            };
+                            copyToTmpfs = mkOption {
+                              default = {};
+                              type = enableModule;
+                            };
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+
+          optionals = mkOption {
+            default = {};
+            type = types.submodule {
+              options = {
+                ricemyttydotcom = mkOption {
+                  default = {};
+                  type = enableModule;
+                };
+              };
+            };
+          };
+        };
+      }));
     };
   };
+
+  # in {
+  #   imports = [./submodules];
+
+  #   options.profiles.kernel = {
+  #     preset = mkOption {
+  #       default = {};
+  #       type = types.attrsOf (types.submodule profileTemplate);
+  #     };
+  #   };
 
   config = mkIf cfg.enable (mkMerge [
     {
