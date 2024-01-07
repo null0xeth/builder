85a547640e6e6ef80fffea87d43f1b7e8f50dd46
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-01-07 15:15:27 +0100

    .

--- parts/profiles/nixos/system-scoped/system/sub-profiles/base-profile.nix
+++ parts/profiles/nixos/system-scoped/system/sub-profiles/base-profile.nix
@@ -6,7 +6,7 @@
 }:
 with lib; let
   base = name: builtins.hasAttr ("${name}" config.profiles.system.preset);
-  filter = lib.filterAttrs (n: _: base n);
+  filter = lib.filterAttrs (n: _: "${base n}");
   filterfunc = builtins.head (builtins.attrNames filter);
   cfg = config.profiles.system.preset."${filterfunc}";
 
