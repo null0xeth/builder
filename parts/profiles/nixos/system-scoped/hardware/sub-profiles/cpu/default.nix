a5bcc1dade8d7c44db3a1d1da2da68042c4bc4a8
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-01-07 07:24:55 +0100

    .

--- parts/profiles/nixos/system-scoped/hardware/sub-profiles/cpu/default.nix
+++ parts/profiles/nixos/system-scoped/hardware/sub-profiles/cpu/default.nix
@@ -13,41 +13,43 @@ with lib; let
   cfg = config.modules.hardware.cpu.${filterfunc config.modules.hardware.cpu};
 in {
   imports = [./submodules];
-  options.modules.hardware.cpu = mkOption {
-    default = {};
-    type = types.attrsOf (types.submodule ({
-      config,
-      name,
-      ...
-    }: {
-      options = {
-        enable = mkEnableOption "the base hardware profile";
-        name = mkOption {
-          type = types.str;
-          description = mdDoc "The slug used to refer to this profile";
-          default = name;
-        };
-        profile = mkOption {
-          type = types.submodule {
-            options = {
-              enable = mkEnableOption "lol";
-              cpu = mkOption {
-                type = types.submodule {
-                  options = {
-                    brand = mkOption {
-                      type = types.nullOr types.str;
-                      description = mdDoc "The manufacturer of your CPU";
-                      #default = null;
-                    };
-                    generation = mkOption {
-                      type = types.nullOr types.int;
-                      description = mdDoc "The generation of your CPU (intel only)";
-                      #default = null;
-                    };
-                    sub-type = mkOption {
-                      type = types.nullOr types.str;
-                      description = mdDoc "The type of CPU installed [desktop|mobile]";
-                      #default = null;
+  options.modules.hardware.cpu = {
+    preset = mkOption {
+      default = {};
+      type = types.attrsOf (types.submodule ({
+        config,
+        name,
+        ...
+      }: {
+        options = {
+          enable = mkEnableOption "the base hardware profile";
+          name = mkOption {
+            type = types.str;
+            description = mdDoc "The slug used to refer to this profile";
+            default = name;
+          };
+          profile = mkOption {
+            type = types.submodule {
+              options = {
+                enable = mkEnableOption "lol";
+                cpu = mkOption {
+                  type = types.submodule {
+                    options = {
+                      brand = mkOption {
+                        type = types.nullOr types.str;
+                        description = mdDoc "The manufacturer of your CPU";
+                        #default = null;
+                      };
+                      generation = mkOption {
+                        type = types.nullOr types.int;
+                        description = mdDoc "The generation of your CPU (intel only)";
+                        #default = null;
+                      };
+                      sub-type = mkOption {
+                        type = types.nullOr types.str;
+                        description = mdDoc "The type of CPU installed [desktop|mobile]";
+                        #default = null;
+                      };
                     };
                   };
                 };
@@ -55,39 +57,39 @@ in {
             };
           };
         };
-      };
-    }));
-  };
-  config = mkIf cfg.enable (mkMerge [
-    {
-      assertions = [
-        {
-          assertion = cfg.settings.generation != 0;
-          message = "Please specify the processor generation. It cannot be omitted";
-        }
-      ];
-    }
-    (mkIf (cfg.profile.enable && (cfg.profile.cpu != null)) {
-      hardware-cpu-presets = {
-        ${cfg.profile.cpu.brand} = {
-          ${cfg.profile.cpu.sub-type} = {
-            "${builtins.toString cfg.profile.cpu.generation}th" = {
-              enable = true;
+      }));
+    };
+    config = mkIf cfg.enable (mkMerge [
+      {
+        assertions = [
+          {
+            assertion = cfg.settings.generation != 0;
+            message = "Please specify the processor generation. It cannot be omitted";
+          }
+        ];
+      }
+      (mkIf (cfg.profile.enable && (cfg.profile.cpu != null)) {
+        hardware-cpu-presets = {
+          ${cfg.profile.cpu.brand} = {
+            ${cfg.profile.cpu.sub-type} = {
+              "${builtins.toString cfg.profile.cpu.generation}th" = {
+                enable = true;
+              };
             };
           };
         };
-      };
-      #   "${cfg.profile.cpu.brand}-${cfg.profile.cpu.sub-type}-${builtins.toString cfg.profile.cpu.generation}th" = {
+        #   "${cfg.profile.cpu.brand}-${cfg.profile.cpu.sub-type}-${builtins.toString cfg.profile.cpu.generation}th" = {
+        #     enable = true;
+        #   };
+      })
+
+      # hardware-cpu-presets = let
+      #   slug = "${cfg.settings.cpuType}-${cfg.settings.sub-type}-${builtins.toString cfg.settings.generation}th";
+      # in {
+      #   ${slug} = {
       #     enable = true;
       #   };
-    })
-
-    # hardware-cpu-presets = let
-    #   slug = "${cfg.settings.cpuType}-${cfg.settings.sub-type}-${builtins.toString cfg.settings.generation}th";
-    # in {
-    #   ${slug} = {
-    #     enable = true;
-    #   };
-    # };
-  ]);
+      # };
+    ]);
+  };
 }
